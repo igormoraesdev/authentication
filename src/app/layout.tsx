99d7d827261937781c0046ab3aaebfc3be8e69ba
Igor Moraes <igormoraesdev@gmail.com>
2025-09-11 19:53:34 -0300

    feat: add public layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { PublicLayout } from '@/components/ui/layout';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -24,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <PublicLayout>{children}</PublicLayout>
+      </body>
     </html>
   );
 }
