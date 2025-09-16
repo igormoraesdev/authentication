049797f5f7e799df2448b428551e7a7d9cee3a80
Igor Moraes <igormoraesdev@gmail.com>
2025-09-16 19:54:06 -0300

    chore: add logs

--- src/lib/auth/server/requireAuth.ts
+++ src/lib/auth/server/requireAuth.ts
@@ -4,18 +4,19 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 const secret = process.env.NEXTAUTH_SECRET;
-if (!secret) throw new Error('NEXTAUTH_SECRET não definido!');
 
 export async function requireAuth(req: NextRequest): Promise<{ user: CustomUser } | NextResponse> {
   try {
     const token = await getToken({ req, secret });
-
+    console.log('token', token);
     if (!token) {
       logger.log('requireAuth: token não encontrado');
       return NextResponse.redirect(new URL('/auth/signin', req.url));
     }
 
     const user = (await jwtService.decode(token.accessToken)) as CustomUser;
+
+    console.log('user', user);
 
     return { user };
   } catch (error) {
