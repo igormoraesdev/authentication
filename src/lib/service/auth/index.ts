5e7e8bcd8cd83dc2f414674cb240f84ecb5a85d3
Igor Moraes <igormoraesdev@gmail.com>
2025-09-15 22:04:52 -0300

    feat: add auth service

---
+++ src/lib/service/auth/index.ts
@@ -0,0 +1,20 @@
+import { httpClient } from '@/lib/api';
+
+export class AuthService {
+  private readonly client = httpClient;
+  private readonly baseEndpoint = '/api/auth';
+
+  async login(credentials: Auth.Params): Promise<Auth.Response> {
+    try {
+      const response = await this.client.post<Auth.Response>(
+        `${this.baseEndpoint}/signin`,
+        credentials,
+      );
+      return response.data;
+    } catch (error) {
+      throw new Error('Credenciais inválidas', { cause: error });
+    }
+  }
+}
+
+export const authService = new AuthService();
